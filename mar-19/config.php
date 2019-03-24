7a3da1eec48862f4ae71b2a28d61e8eec798376f
u3166303 <47630636+u3166303@users.noreply.github.com>
2019-03-24 20:34:35 +1100

    Add files via upload

---
+++ mar-19/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host       = "localhost";
+$username   = "root";
+$password   = "root";
+$dbname     = "ben_test"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
